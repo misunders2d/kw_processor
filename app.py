3c2c1202a907d8cb256eb406fc1d29dea4045a2a
MISUNDERSTOOD <2djohar@gmail.com>
2023-01-25 16:38:13 +0200

    added date slider

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ Created on Thu Dec 29 20:36:08 2022
 """
 
 import streamlit as st
+from datetime import datetime
 import pandas as pd
 add_selectbox = st.sidebar.selectbox(
     "How would you like to be contacted?",
@@ -21,5 +22,11 @@ if cerebro_file:
     asin_list = ', '.join(asins) + ' '
     st.write(asin_list)
     st.write('Done')
-    st.write('Love you :heart:')
+date1,date2 = st.slider(
+    "Select date range",
+    min_value = datetime(2020,1,1), max_value = datetime(2023,1,1),
+    value=(datetime(2021,1,1),datetime(2022,1,1)),
+    format="MM/DD/YY", 
+    )
+st.write("Start time:", date1.strftime("%Y-%m-%d"),' - ', date2.strftime("%Y-%m-%d"))
     
